074c55f96db11f07466460010f628f706dd6212b
bxljoy <259500313@qq.com>
2024-07-04 01:33:37 +0200

    feat: Add MobileNav component for mobile navigation links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-      {/* <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn> */}
     </div>
   );
 };
